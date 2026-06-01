fc5aa6b477332c662de2ba46101c662cea59539b
yan-dev11 <0000111887044xsp@al.educacao.sp.gov.br>
2026-06-01 16:21:03 -0300

    ufa

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(const TodoListApp());
+}
+
+class TodoListApp extends StatelessWidget {
+  const TodoListApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
